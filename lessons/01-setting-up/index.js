4fd4edc265d38862e5f5d8336a8265e8e0bc4296
klishevich <busation@gmail.com>
2016-07-10 17:06:06 +0300

    route links from homepage

--- lessons/01-setting-up/index.js
+++ lessons/01-setting-up/index.js
@@ -1,4 +1,15 @@
 import React from 'react'
 import { render } from 'react-dom'
 import App from './modules/App'
-render(<App/>, document.getElementById('app'))
+import About from './modules/About'
+import Repos from './modules/Repos'
+import { Router, Route, hashHistory } from 'react-router'
+
+render((
+  <Router history={hashHistory}>
+    <Route path="/" component={App}/>
+    {/* add the routes here */}
+    <Route path="/repos" component={Repos}/>
+    <Route path="/about" component={About}/>
+  </Router>
+), document.getElementById('app'))
--- lessons/01-setting-up/modules/App.js
+++ lessons/01-setting-up/modules/App.js
@@ -1,7 +1,16 @@
 import React from 'react'
+import { Link } from 'react-router'
 
 export default React.createClass({
   render() {
-    return <div>Hello, React Router 2!</div>
+    return (
+    	<div>
+    		<h1>Hello, React Router 2!</h1>
+    		<ul role="nav">
+    			<li><Link to="/about">About</Link></li>
+    			<li><Link to="/repos">Repos</Link></li>
+    		</ul>
+    	</div>
+    	)
   }
 })
